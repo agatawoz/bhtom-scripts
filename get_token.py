76fd7c978623d567dd700551f42318a5189a6a97
agatawoz <agatex2003@gmail.com>
2025-07-09 21:42:16 +0200

    Add script for obtaining token via API

--- get_token.py
+++ get_token.py
@@ -1,9 +1,33 @@
 '''
-This script allows to get a token for the BHTOM API.
+This script allows to get a token for the BHTOM API. It writes the token to a file called "token.txt".
 
 '''
 
 
-import os
 import requests
-import argparse
+import getpass
+
+#Login to BHTOM account
+print("Log to your BHTOM account:")
+username = input("Login: ")
+password = getpass.getpass("Password: ")
+
+url = "https://bh-tom2.astrolabs.pl/api/token-auth/"
+
+#Send request
+response = requests.post(
+    url,
+    json={"username": username, "password": password}
+)
+
+#Answer handling
+if response.status_code == 200:
+    token = response.json().get("token")
+    print("Your token:", token)
+
+    #Write token to file
+    with open("token.txt", "w") as f:
+        f.write(token)
+
+else:
+    print("Error:", response.status_code)
