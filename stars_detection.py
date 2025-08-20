1fcc6518161f4a7105b5ac8f10885f06e0e63583
agatawoz <agatex2003@gmail.com>
2025-08-20 22:52:13 +0200

    Add function to detect objects

--- stars_detection.py
+++ stars_detection.py
@@ -1,28 +1,39 @@
+'''
+
+This script allows to detect objects in a FITS file using the Photutils library in Python.
+
+'''
+
 from astropy.io import fits
 from astropy.stats import sigma_clipped_stats
 import numpy as np
 from photutils.detection import DAOStarFinder
 import os
 
-#Read the FITS file from the current directory
-current_dir = os.path.dirname(__file__)
-file_path = os.path.join(current_dir, 'test.fits')
-hdul = fits.open(file_path)
+file_name = '2024-09-30_06-42-09_PKS0454-234_V_180.00s_1x1_0_0286_out.fits'
+def detect_stars(file_name):
 
-#Get the data from the FITS file
-data = hdul[0].data
+    #Read the FITS file from the current directory
+    current_dir = os.path.dirname(__file__)
+    file_path = os.path.join(current_dir, file_name)
+    hdul = fits.open(file_path)
 
-#Close the FITS file
-hdul.close()
+    #Get the data from the FITS file
+    data = hdul[0].data
+    header = hdul[0].header
+    object_name = header['OBJECT']
 
-#Calculate the mean, median, and standard deviation of the data
-mean, median, std = sigma_clipped_stats(data)
+    #Close the FITS file
+    hdul.close()
 
-#Detect sources in the data
-daofind = DAOStarFinder(fwhm=3.0, threshold=5.*std)  
-sources = daofind(data - median) 
+    #Calculate the mean, median and standard deviation of the data
+    mean, median, std = sigma_clipped_stats(data)
 
-if sources is not None and len(sources) > 0:
-    print(f" {len(sources)} stars decetcted.")
-else:
-    print("No stars decected")
+    #Detect sources in the data
+    daofind = DAOStarFinder(fwhm=3.0, threshold=5.*std)  
+    sources = daofind(data - median) 
+
+    if sources is not None and len(sources) > 10:
+        return (object_name, len(sources))
+    else:
+        return (object_name, 0)
