20e1e006599318d649f9d1df9a1c99b20dcbf3cf
agatawoz <agatex2003@gmail.com>
2025-08-24 20:34:26 +0200

    Add function for mapping object names

--- stars_detection.py
+++ stars_detection.py
@@ -9,31 +9,36 @@ from astropy.stats import sigma_clipped_stats
 import numpy as np
 from photutils.detection import DAOStarFinder
 import os
+import yaml
 
-file_name = '2024-09-30_06-42-09_PKS0454-234_V_180.00s_1x1_0_0286_out.fits'
-def detect_stars(file_name):
+def detect_stars(file_path, min_source=10):
 
-    #Read the FITS file from the current directory
-    current_dir = os.path.dirname(__file__)
-    file_path = os.path.join(current_dir, file_name)
-    hdul = fits.open(file_path)
+    #get the data from the FITS file
+    with fits.open(file_path) as hdul:
+        data = hdul[0].data
+        header = hdul[0].header
 
-    #Get the data from the FITS file
-    data = hdul[0].data
-    header = hdul[0].header
-    object_name = header['OBJECT']
+    object_name = header.get('OBJECT', 'Unknown')
 
-    #Close the FITS file
-    hdul.close()
-
-    #Calculate the mean, median and standard deviation of the data
+    #calculate the mean, median and standard deviation of the data
     mean, median, std = sigma_clipped_stats(data)
 
-    #Detect sources in the data
+    #detect sources in the data
     daofind = DAOStarFinder(fwhm=3.0, threshold=5.*std)  
     sources = daofind(data - median) 
 
-    if sources is not None and len(sources) > 10:
+    if sources > min_source:
         return (object_name, len(sources))
     else:
         return (object_name, 0)
+
+
+def rotuz_to_bhtom_name(rotuz_name, file="bhtom_objects.yaml"):
+    try:
+        with open(file) as f:
+            names = yaml.safe_load(f)
+            print(f)
+            return names.get(rotuz_name, rotuz_name)
+    except FileNotFoundError:
+        print(f"File '{file}' not found.")
+        return None
