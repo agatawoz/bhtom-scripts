62ac8b9d1c5a50236cc2843c9eec78e9f4445687
agatawoz <agatex2003@gmail.com>
2025-09-01 21:31:54 +0200

    Add script for obtaining token via API

--- get_token.py
+++ get_token.py
@@ -1,33 +1,37 @@
 '''
-This script allows to get a token for the BHTOM API. It writes the token to a file called "token.txt".
+This script allows to get a token for the BHTOM API. 
 
 '''
 
-
 import requests
-import getpass
-
-#Login to BHTOM account
-print("Log to your BHTOM account:")
-username = input("Login: ")
-password = getpass.getpass("Password: ")
+import argparse
 
 url = "https://bh-tom2.astrolabs.pl/api/token-auth/"
 
-#Send request
-response = requests.post(
-    url,
-    json={"username": username, "password": password}
-)
 
-#Answer handling
-if response.status_code == 200:
-    token = response.json().get("token")
-    print("Your token:", token)
+def main():
+    
+    parser = argparse.ArgumentParser("Login to your BHTOM account")
+    parser.add_argument('--login', default=None, help='Your BHTOM login')
+    parser.add_argument('--password', default=None, help='Your BHTOM password')
 
-    #Write token to file
-    with open("token.txt", "w") as f:
-        f.write(token)
+    args = parser.parse_args()
+    login = args.login
+    password = args.password
 
-else:
-    print("Error:", response.status_code)
+
+    #send request
+    response = requests.post(
+        url,
+        json={"username": login, "password": password}
+    )
+
+    #answer handling
+    if response.status_code == 200:
+        token = response.json().get("token")
+        print("Your token:", token)
+    else:
+        print("Error:", response.status_code)
+
+if __name__ == "__main__":
+    main()
